bf580e14ceaede90077cc91e9ef8c21d3dc18b1d
ameemameme <ameemameme@gmail.com>
2025-11-12 21:15:23 +0500

    demo.py added

--- demo.py
+++ demo.py
@@ -1,3 +1,18 @@
 print("Unwelcome Abode")
-print("What even")
-print("ewfbneiueru")
+print()
+print("If it is in bracket & capitalised, type it inside quotes")
+print("If it is in bracket just do what it says")
+print("Make new terminal and type; git clone (your repository URL code)")
+print("git status")
+print("cd (FOLDER NAME)")
+print("git add (FILE NAME)")
+print("git commit -m (FILE NAME added)")
+print("git push origin main")
+print()
+horizontal=int(input("Enter a value for horizontal:"))
+vertical=int(input("Enter a value for vertical:"))
+for i in range(horizontal):
+    for j in range (vertical):
+        if j==0 or i==0:
+            print("*",end=" ")
+    print()
